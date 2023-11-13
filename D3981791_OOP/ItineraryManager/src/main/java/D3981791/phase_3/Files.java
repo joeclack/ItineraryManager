b2f366cc30bd277ba69c0858126628b4b7ec2dde
Joe Clack <joebclack@gmail.com>
2023-11-13 11:59:44 +0000

    Serializable

--- D3981791_OOP/ItineraryManager/src/main/java/D3981791/phase_3/Files.java
+++ D3981791_OOP/ItineraryManager/src/main/java/D3981791/phase_3/Files.java
@@ -7,59 +7,29 @@ import java.io.IOException;
 public class Files {
 
     public static void SaveToFile(Itinerary itinerary){
-        // Each row of the file will represent 1 itinerary, where each field is separated by a tab.
-        //•	Reference
-        //•	Date
-        //•	Attendee name
-        //•	Total cost (in pence)
-        //•	Total activities
-        //•	Activities code (format 3 letter, hyphen, 2 digits)
-        // 	Where add-ons have been selected, the activity code is followed colon and then a comma separated list of add-on codes, eg:
-        //SWM-01:INS,TRN
-        //
-        //Where INS means insurance and TRN is the code for transport.  You are welcome to make up your own codes.
 
-        String fileName = "itineraries.txt";
+        String filename = "file.ser";
 
-        String content = "Hello, World!"; // Content to be written to the file
+        // Serialization
+        try
+        {
+            //Saving of object in a file
+            FileOutputStream file = new FileOutputStream(filename);
+            ObjectOutputStream out = new ObjectOutputStream(file);
 
-        StringBuilder itineraryString = new StringBuilder();
-        itineraryString.append(itinerary.getRefNumber())
-                .append(", ")
-                .append(itinerary.getAttendeeInst().getDate())
-                .append(", ")
-                .append(itinerary.getAttendeeInst().getFirstName())
-                .append(" ")
-                .append(itinerary.getAttendeeInst().getLastName())
-                .append(", ")
-                .append(itinerary.getTotalCost() - ((itinerary.getTotalCost() * ((itinerary.getDiscount())))/100))
-                .append(", ")
-                .append(itinerary.getAttendeeInst().getTotalActivities())
-                .append(", ");
+            // Method for serialization of object
+            out.writeObject(itinerary);
 
-        int i = 0;
-        int j = 0;
+            out.close();
+            file.close();
 
-        for(Activity activity : itinerary.getActivitiesList()) {
-            itineraryString.append(itinerary.getActivitiesList().get(i).getActivityCode())
-                    .append(": ");
-            for(ActivityAddOn addOn : itinerary.getActivitiesList().get(j).getActivityAddOnList()) {
-                itineraryString.append(addOn.getCode())
-                        .append(", ");
-                j++;
-            }
-            i++;
+            System.out.println("Object has been serialized");
+
         }
 
-
-
-
-
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true))) {
-            writer.write(String.valueOf(itineraryString)); // Write the content to the file
-            System.out.println("Content has been written to the file.");
-        } catch (IOException e) {
-            System.err.println("Error writing to the file: " + e.getMessage());
+        catch(IOException ex)
+        {
+            System.out.println("IOException is caught");
         }
 
     }
